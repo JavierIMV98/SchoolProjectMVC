91829ad0a09f6c23885f0d0c22ff76b1176dcc54
JavierIMV <moyavalerio.ji@gmail.com>
2023-01-25 10:38:38 -0300

    DB and Scaffolded Views and Controllers

---
+++ Data/SchoolManagementDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolProjectMVC.Data;
+
+public partial class SchoolManagementDbContext : DbContext
+{
+    public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC07B20E093A");
+
+            entity.HasIndex(e => e.Code, "UQ__Courses__A25C5AA7002817D1").IsUnique();
+
+            entity.Property(e => e.Code).HasMaxLength(5);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC07F23AB144");
+
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC070AE7E874");
+
+            entity.Property(e => e.DateOfBirth).HasColumnType("date");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
